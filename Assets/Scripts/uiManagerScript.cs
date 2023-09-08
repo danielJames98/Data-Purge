00624e373f426ebba24325c42a500dc5472fe561
danielJames98 <daniel.a.james.1998@gmail.com>
2023-09-08 17:05:03 +0100

    Fixed missing filters

--- Assets/Scripts/uiManagerScript.cs
+++ Assets/Scripts/uiManagerScript.cs
@@ -2,7 +2,7 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.Playables;
+//using UnityEditor.Playables;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
